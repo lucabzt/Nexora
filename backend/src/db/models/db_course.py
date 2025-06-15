f41eae641b2b0908b36f18e6e6b222620fe081e8
Markus <git+markus@obermui.de>
2025-06-15 14:34:35 +0200

    fix deletion of courses with notes

--- backend/src/db/models/db_course.py
+++ backend/src/db/models/db_course.py
@@ -63,6 +63,7 @@ class Chapter(Base):
     # Relationships
     course = relationship("Course", back_populates="chapters")
     mc_questions = relationship("MultipleChoiceQuestion", back_populates="chapter", cascade="all, delete-orphan")
+    notes = relationship("Note", back_populates="chapter", cascade="all, delete-orphan")
 
     # This makes ordering chapters by their index for a given course very fast.
     __table_args__ = (
