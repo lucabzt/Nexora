bdd1de9c898eaca30d9fa92e919f08585f7169ae
Markus <git+markus@obermui.de>
2025-06-15 16:45:16 +0200

    fix langeauge query

--- backend/src/services/query_service.py
+++ backend/src/services/query_service.py
@@ -24,7 +24,7 @@ class QueryService:
         return create_text_query(pretty_chapter)
 
 
-    def get_explainer_query(self, user_id, course_id, chapter_idx):
+    def get_explainer_query(self, user_id, course_id, chapter_idx, language, difficulty):
         chapter = self.sm.get_state(user_id, course_id)['chapters'][chapter_idx]
         pretty_chapter = \
             f"""
@@ -33,8 +33,8 @@ class QueryService:
                 Time in Minutes: {chapter['time']}
                 Content Summary: \n{json.dumps(chapter['content'], indent=2)}
                 Note by Planner Agent: {json.dumps(chapter['note'], indent=2)}
-                Response Language: {chapter['language']}
-                Response Difficulty: {chapter['difficulty']}
+                Response Language: {language}
+                Response Difficulty: {difficulty}
             """
         return create_text_query(pretty_chapter)
 
