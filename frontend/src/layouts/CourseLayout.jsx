b52f053683fe0b82d109d1d2775cb87c6befeb6c
Matthias Meierlohr <matthi24.m@web.de>
2025-06-16 17:09:29 +0200

    fixed main link error

--- frontend/src/layouts/CourseLayout.jsx
+++ frontend/src/layouts/CourseLayout.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { MainLink } from './AppLayout';
 import { Outlet, Link as RouterLink, useNavigate, useLocation, useParams } from 'react-router-dom';
 import {
   AppShell,
